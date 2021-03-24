efff2dfc834a47df0d0b16c70bfc159678357018
mayumiasakawa <63479656+mayumiasakawa@users.noreply.github.com>
2021-03-24 21:40:46 +0900

    add to App.vue script/style

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,62 @@
 </template>
 
 <script>
-
+  export default{
+    name: "App"
+  };
 </script>
 
 <style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 
+#app {
+  margin: 0;
+  padding: 0;
+}
+
+.links {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  list-style: none;
+}
+
+.links > li > a {
+  display: block;
+  color: #666;
+  padding: 5px 20px;
+  text-decoration: none;
+  transition: background 0.4s;
+}
+.links > li > a:hover {
+  background: #f6f6f6;
+}
+.links > li > a.router-link-active {
+  background: transparent;
+  color: teal;
+  cursor: default;
+}
+
+.page {
+  position: relative;
+}
+.page > * {
+  position: absolute;
+}
+
+.router-enter-active {
+  transition: all 1s;
+  transition-delay: 0.5s;
+}
+.router-leave-active {
+  transition: all 0.5s;
+}
+.router-enter,
+.router-leave-to {
+  opacity: 0;
+}
 </style>
